fec40156026b4de87d4f33e544c6cf0748ff7bf7
UpriseAkin <akin@uprisepartners.com>
2021-05-01 16:57:52 -0400

    dev

--- pages/api/load-products.js
+++ pages/api/load-products.js
@@ -1,8 +1,11 @@
 export async function loadProducts() {
-    const data = await fetch('http://.netlify/functions/get-products')
+    const data = await fetch('http://www.nba.com')
       .then((res) => res.json())
       .catch((err) => console.error(err));
 
       return data;
   
   }
+
+
+  // const data = await fetch('http://localhost:8888/.netlify/functions/get-products')
