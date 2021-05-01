69abe1dc96e9bb0e6108f2c75f84cb18a94a7a6b
UpriseAkin <akin@uprisepartners.com>
2021-05-01 03:06:03 -0400

    initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return (
+  <Auth0Provider
+    domain="dev-azwil67y.us.auth0.com"
+    clientId="GxRSF2rhyFgmYk29IOyrkgNj85vFQbHl"
+    redirectUri={"http://localhost:3000"}
+  >
+    <Component {...pageProps} />
+  </Auth0Provider>
+  ) 
 }
 
 export default MyApp
