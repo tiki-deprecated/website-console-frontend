27cf7bcbb222ea732ee429b5ec806c58d728ee68
UpriseAkin <akin@uprisepartners.com>
2021-05-01 17:26:54 -0400

    remove function

--- pages/payment.js
+++ pages/payment.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { loadProducts } from './api/load-products.js';
+// import { loadProducts } from './api/load-products.js';
 import styles from '../styles/Home.module.css'
 
 export default function Payment(){
-    let products = loadProducts()
-    console.log(products)
+    // let products = loadProducts()
+    // console.log(products)
 
-    // console.log(loadProducts());
-    if (products) {
-        return 'yes'
-    } else {
-        return 'loading...'
-    }
+    // // console.log(loadProducts());
+    // if (products) {
+    //     return 'yes'
+    // } else {
+    //     return 'loading...'
+    // }
 }
