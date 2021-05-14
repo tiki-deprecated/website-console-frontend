2456785f025d5ae10d33fb6f3a8c6e287559c5c4
Anthony Dirago <anthony@uprisepartners.com>
2021-05-14 14:54:38 -0400

    Update Stripe Package to Latest Version

--- components/CheckoutForm.js
+++ components/CheckoutForm.js
@@ -1,7 +1,110 @@
-import { CardElement, injectStripe } from 'react-stripe-elements-universal';
-import React, { useState } from 'react';
+import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import React, { useMemo, useState } from "react";
 
 import styles from './CheckoutForm.module.css';
+
+const useOptions = () => {
+  const options = useMemo(
+    () => ({
+      style: {
+        base: {
+          fontSize: "18px",
+          color: "#424770",
+          letterSpacing: "0.025em",
+          fontFamily: "Source Code Pro, monospace",
+          "::placeholder": {
+            color: "#aab7c4"
+          }
+        },
+        invalid: {
+          color: "#9e2146"
+        }
+      }
+    }),
+  );
+
+  return options;
+};
+
+const CheckoutForm = () => {
+  const stripe = useStripe();
+  const elements = useElements();
+  const options = useOptions();
+  const [status, setStatus] = useState('default');
+
+  const handleSubmit = async event => {
+    event.preventDefault();
+
+    if (!stripe || !elements) {
+      // Stripe.js has not loaded yet. Make sure to disable
+      // form submission until Stripe.js has loaded.
+      return;
+    }
+    setStatus('submitting');
+
+    //This method should be updated to create a customer as well as a charge.
+    try {
+      let { token } = await stripe.createToken({ name: 'Name' });
+
+      let response = await fetch('/.netlify/functions/charge', {
+        method: 'POST',
+        body: JSON.stringify({
+          amount: totalCost * 100,
+          token: token.id,
+        }),
+      });
+
+      if (response.ok) {
+        setStatus('complete');
+      } else {
+        throw new Error('Network response was not ok.');
+      }
+    } catch (err) {
+      setStatus('error');
+    }
+  };
+  
+
+  if (status === 'complete') {
+    return <div className={styles.CheckoutFormComplete}>Payment successful!</div>;
+  }
+
+  return (
+    <div className={styles.checkoutWrapper}>
+    <form onSubmit={handleSubmit} className={styles.stripeCheckout}>
+      <label>
+        Card details
+        <CardElement
+          options={options}
+          onReady={() => {
+            console.log("CardElement [ready]");
+          }}
+          onChange={event => {
+            console.log("CardElement [change]", event);
+          }}
+          onBlur={() => {
+            console.log("CardElement [blur]");
+          }}
+          onFocus={() => {
+            console.log("CardElement [focus]");
+          }}
+        />
+      </label>
+      <button type="submit" disabled={!stripe} disabled={status === 'submitting'}>
+        {status === 'submitting' ? 'Submitting' : 'Pay $1'}
+      </button>
+      {status === 'error' && (
+          <div className={styles.CheckoutFormError}>Something went wrong.</div>
+        )}
+    </form>
+    </div>
+  );
+};
+
+export default CheckoutForm;
+
+/* 
+Old Method of Checkout-------------
 
 function CheckoutForm({ stripe, totalCost }) {
   const [status, setStatus] = useState('default');
@@ -57,4 +160,4 @@ function CheckoutForm({ stripe, totalCost }) {
   );
 }
 
-export default injectStripe(CheckoutForm);
+export default injectStripe(CheckoutForm); */
--- pages/payment.js
+++ pages/payment.js
@@ -1,23 +1,18 @@
-import React from 'react';
-import Head from "next/head";
-import { Elements, StripeProvider } from 'react-stripe-elements-universal';
 import CheckoutForm from '../components/CheckoutForm';
+import { Elements } from '@stripe/react-stripe-js';
+import React from 'react';
+import { loadStripe } from '@stripe/stripe-js';
 // import { loadProducts } from './api/load-products.js';
 import styles from '../styles/Home.module.css'
 
-export default function Payment(){
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY);
+
+export default function Payment() {
     return (
         <>
-            <>
-                <Head>
-                    <script  src="https://js.stripe.com/v3/" />
-                </Head>
-            </>
-            <StripeProvider apiKey={process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}>
-                <Elements>
-                    <CheckoutForm totalCost={1} />
-                </Elements>
-            </StripeProvider>
+            <Elements stripe={stripePromise}>
+                <CheckoutForm totalCost={1} />
+            </Elements>
         </>
     )
 }
