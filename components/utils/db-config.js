a9c5312b08a71bc239bfe074f8b5ba120b0d7386
UpriseAkin <akin@uprisepartners.com>
2021-05-20 10:56:53 -0400

    formatting

--- components/utils/db-config.js
+++ components/utils/db-config.js
@@ -5,10 +5,10 @@ const certPath = path.join(__dirname, '/ca.pem').replace('.next/server/', '');
 
 
 module.exports = {
-        database: process.env.DB_NAME,
-        host: process.env.DB_HOST,
-        password: process.env.DB_PASSWORD,
-        port: process.env.DB_PORT,
-        ssl: { ca: fs.readFileSync(certPath) },
-        user: process.env.DB_USER,
-    };
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: { ca: fs.readFileSync(certPath) },
+    user: process.env.DB_USER,
+};
