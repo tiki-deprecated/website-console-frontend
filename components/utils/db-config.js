eec95b49317efc90530449f79f5b245f1b0f8a55
UpriseAkin <akin@uprisepartners.com>
2021-05-20 10:56:08 -0400

    dynamic cert path

--- components/utils/db-config.js
+++ components/utils/db-config.js
@@ -1,10 +1,14 @@
 const fs = require('fs');
+const path = require('path');
+
+const certPath = path.join(__dirname, '/ca.pem').replace('.next/server/', '');
+
 
 module.exports = {
         database: process.env.DB_NAME,
         host: process.env.DB_HOST,
         password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
-        ssl: { ca: fs.readFileSync('/home/akin/dev/tiki/expert-hub/server-side-approach/biz-app/pages/api/ca.pem') },
+        ssl: { ca: fs.readFileSync(certPath) },
         user: process.env.DB_USER,
     };
