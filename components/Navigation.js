18296c51d750901ca8a5deba0b6cadda67c8c97b
Anthony Dirago <anthony@uprisepartners.com>
2021-05-20 14:59:24 -0400

    Add previous updates to new branch

---
+++ components/Navigation.js
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+function Navigation() {
+
+
+    return (
+        <div className="navigation">
+            <div className="navigationContainer">
+                <ul>
+                    <li>
+                        <Link className="logo" href="/">
+                            <a><Image src='/TIKI_logo_black.png' width={100} height={33}/>
+                            </a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/dashboard">
+                            <a className="navLink">Dashboard</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/payment">
+                            <a className="navLink">Payment</a>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/application">
+                            <a className="navLink">Application</a>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+
+    );
+}
+
+export default Navigation;
