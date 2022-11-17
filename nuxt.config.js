586cefd0a856ad7286b105ef6c9fc8226179cf2e
Mike Audi <mike@mytiki.com>
2022-11-17 00:05:38 -0600

    hosting finally updated it. changing back to proper naming. it'll sort itself out eventually

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,7 +16,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/fav.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
   css: [
     '@/assets/fonts/koara/stylesheet.css',
