29acd50613292b7d62679daafd11857e4a74bdc1
Mike Audi <mike@mytiki.com>
2023-01-30 15:21:51 -0600

    fix(#35): implement login layout
    closes #35

--- pages/login.vue
+++ pages/login.vue
@@ -4,13 +4,30 @@
   -->
 
 <template>
-  <NuxtLayout name="login-layout">
-    <FormLoginCmp />
-  </NuxtLayout>
+  <single-input-form
+    title="Sign in to your account"
+    subtitle="Or sign up; it's
+  free."
+    cta="Continue"
+    label="Email address"
+    :input="input"
+    :onSubmit="onSubmit"
+  />
 </template>
 
 <script setup lang="ts">
 definePageMeta({
   layout: 'login-layout',
 })
+
+const input: HTMLInputElement = {
+  name: 'email',
+  type: 'email',
+  autocomplete: 'email',
+  placeholder: 'hello@mytiki.com',
+}
+
+const onSubmit = () => {
+  console.log('sucka')
+}
 </script>
