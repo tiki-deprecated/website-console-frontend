4e34c7e8460474b747831f60e798c986b575bd36
Mike Audi <mike@mytiki.com>
2023-02-01 23:20:27 -0600

    feat(#43)
    adds silent refresh using refresh_tokens (w/ rotation) in HTTP Only & Secure cookies

---
+++ worker/refresh/src/l0-auth.ts
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) TIKI Inc.
+ * MIT license. See LICENSE file in root directory.
+ */
+
+export { refresh }
+
+interface L0AuthRspToken {
+  access_token: string
+  refresh_token: string
+  scope: string
+  token_type: string
+  expires_in: number
+}
+
+async function refresh(
+  url: string,
+  refreshToken: string
+): Promise<L0AuthRspToken> {
+  console.log(url)
+  console.log(refreshToken)
+  const response: Response = await fetch(url, {
+    method: 'post',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Accept: 'application/json',
+    },
+    body: new URLSearchParams({
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+    }),
+  }).catch((error) => {
+    console.log(error)
+    return Promise.reject(error)
+  })
+  if (response.ok) return await response.json()
+  else {
+    console.log(await response.json())
+    return Promise.reject(response.status)
+  }
+}
